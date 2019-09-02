adb0fae8fa6f21e3c4e243e865145f4d8495d1a2
abhigupta19 <gabhi8740@gmail.com>
2019-09-02 18:48:30 +0530

    Initial commit

---
+++ app/src/main/java/com/sar/user/webview/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sar.user.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView=findViewById(R.id.ee);
+        webView.loadUrl("http://www.google.com");
+        webView.canGoBack();
+        webView.setWebViewClient(new WebViewClient());
+
+    }
+}
